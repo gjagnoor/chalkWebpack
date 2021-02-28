4f6141d3d73bfc98beed7b3183b60bc918597ce0
jgrewal <jagnoorg@jagnoorgs-MacBook-Pro.local>
2021-03-01 00:33:09 +0530

    improved process and flexible

--- chalkWebpack.js
+++ chalkWebpack.js
@@ -2,16 +2,198 @@ const fs = require("fs");
 const path = require("path");
 const chalk = require("chalk");
 
-let colors = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
-		  '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
-		  '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
-		  '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
-		  '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
-		  '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
-		  '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', 
-		  '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
-		  '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
-      '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
+let CSS_COLOR_NAMES = [
+    "AliceBlue",
+    "AntiqueWhite",
+    "Aqua",
+    "Aquamarine",
+    "Azure",
+    "Beige",
+    "Bisque",
+    "Black",
+    "BlanchedAlmond",
+    "Blue",
+    "BlueViolet",
+    "Brown",
+    "BurlyWood",
+    "CadetBlue",
+    "Chartreuse",
+    "Chocolate",
+    "Coral",
+    "CornflowerBlue",
+    "Cornsilk",
+    "Crimson",
+    "Cyan",
+    "DarkBlue",
+    "DarkCyan",
+    "DarkGoldenRod",
+    "DarkGray",
+    "DarkGrey",
+    "DarkGreen",
+    "DarkKhaki",
+    "DarkMagenta",
+    "DarkOliveGreen",
+    "DarkOrange",
+    "DarkOrchid",
+    "DarkRed",
+    "DarkSalmon",
+    "DarkSeaGreen",
+    "DarkSlateBlue",
+    "DarkSlateGray",
+    "DarkSlateGrey",
+    "DarkTurquoise",
+    "DarkViolet",
+    "DeepPink",
+    "DeepSkyBlue",
+    "DimGray",
+    "DimGrey",
+    "DodgerBlue",
+    "FireBrick",
+    "FloralWhite",
+    "ForestGreen",
+    "Fuchsia",
+    "Gainsboro",
+    "GhostWhite",
+    "Gold",
+    "GoldenRod",
+    "Gray",
+    "Grey",
+    "Green",
+    "GreenYellow",
+    "HoneyDew",
+    "HotPink",
+    "IndianRed",
+    "Indigo",
+    "Ivory",
+    "Khaki",
+    "Lavender",
+    "LavenderBlush",
+    "LawnGreen",
+    "LemonChiffon",
+    "LightBlue",
+    "LightCoral",
+    "LightCyan",
+    "LightGoldenRodYellow",
+    "LightGray",
+    "LightGrey",
+    "LightGreen",
+    "LightPink",
+    "LightSalmon",
+    "LightSeaGreen",
+    "LightSkyBlue",
+    "LightSlateGray",
+    "LightSlateGrey",
+    "LightSteelBlue",
+    "LightYellow",
+    "Lime",
+    "LimeGreen",
+    "Linen",
+    "Magenta",
+    "Maroon",
+    "MediumAquaMarine",
+    "MediumBlue",
+    "MediumOrchid",
+    "MediumPurple",
+    "MediumSeaGreen",
+    "MediumSlateBlue",
+    "MediumSpringGreen",
+    "MediumTurquoise",
+    "MediumVioletRed",
+    "MidnightBlue",
+    "MintCream",
+    "MistyRose",
+    "Moccasin",
+    "NavajoWhite",
+    "Navy",
+    "OldLace",
+    "Olive",
+    "OliveDrab",
+    "Orange",
+    "OrangeRed",
+    "Orchid",
+    "PaleGoldenRod",
+    "PaleGreen",
+    "PaleTurquoise",
+    "PaleVioletRed",
+    "PapayaWhip",
+    "PeachPuff",
+    "Peru",
+    "Pink",
+    "Plum",
+    "PowderBlue",
+    "Purple",
+    "RebeccaPurple",
+    "Red",
+    "RosyBrown",
+    "RoyalBlue",
+    "SaddleBrown",
+    "Salmon",
+    "SandyBrown",
+    "SeaGreen",
+    "SeaShell",
+    "Sienna",
+    "Silver",
+    "SkyBlue",
+    "SlateBlue",
+    "SlateGray",
+    "SlateGrey",
+    "Snow",
+    "SpringGreen",
+    "SteelBlue",
+    "Tan",
+    "Teal",
+    "Thistle",
+    "Tomato",
+    "Turquoise",
+    "Violet",
+    "Wheat",
+    "White",
+    "WhiteSmoke",
+    "Yellow",
+    "YellowGreen",
+];
+
+// eslint-disable-next-line no-unused-vars
+CSS_COLOR_NAMES = CSS_COLOR_NAMES.map((color) => color.toLowerCase());
+
+module.exports = class chalkwebpack {
+    apply(compiler) {
+        compiler.hooks.afterEmit.tap("chalkwebpack", stats => {
+        // const { path, filename } = stats.compilation.options.output;
+        try {
+            // let filePath = path + "/" + filename;
+            let directoryPath = path.resolve(`${__dirname}/build/static/js/`);
+            let files = getAllFiles(directoryPath);
+            files.forEach((filePath) => {
+                fs.readFile(filePath, "utf8", (err, data) => {
+                    if (data) {
+                        const rgx = /console.log\(['|"](.*?)['|"]\)/g;
+                        let arr = ['console.log(', ', ', ')', ","]
+                        let strarr = data.match(rgx);
+                        let strarr_ = strarr ? strarr.map((str) => [str.slice().split('"').filter((ele, i) => !arr.includes(ele) && i !== str.slice().split('"').length-2 ), str.split('"')[str.split('"').length-2] || null]) : null;
+                        let newdata = data;
+                        if (strarr && strarr_ && strarr.length) {
+                            for (var i = 0; i < strarr.length; i++) {
+                            newdata = newdata.replace(`${strarr[i]}`, `console.log("%c ${strarr_[i][0]}", 'color: ${CSS_COLOR_NAMES.includes(strarr_[i][1]) || (/^#[0-9A-F]{6}$/i.test(strarr_[i][1])) ? strarr_[i][1] : "lightblue" || "lightblue"};')`);
+                            console.log(chalk.blue("logs are now colorful!"))
+                            if (err) console.log(err);
+                            fs.writeFile(filePath, newdata, function (err) {
+                            if (err) {
+                                return console.log(err)
+                            } 
+                            });
+                          }
+                        }
+
+                    }
+                });
+            })
+        } catch (error) {
+            console.log(error)
+        }
+        });
+    }
+};
       
 const getAllFiles = function(dirPath, arrayOfFiles) {
   let files = fs.readdirSync(dirPath)
@@ -27,40 +209,3 @@ const getAllFiles = function(dirPath, arrayOfFiles) {
   })
   return arrayOfFiles
 }
-
-class chalkWebpack {
-  apply(compiler) {
-      compiler.hooks.thisCompilation.tap("chalkWebpack", compilation => {
-          let directoryPath = path.join(`${__dirname}`, "../../", `src/`);
-        let files = getAllFiles(directoryPath);
-        files.forEach((filePath) => {
-          fs.readFile(filePath, "utf8", (err, data) => {
-            if (data) {
-              const rgx = /console.log\(['|"](.*?)['|"]\)/g;
-              let strarr = data.match(rgx);
-              let strarr_ = strarr ? strarr.map((str) => str.split('"')[1]) : null;
-              let newdata = data;
-              if (strarr && strarr_ && strarr.length) {
-                for (var i = 0; i < strarr.length; i++) {
-                  if (strarr_[i].split("%c").length > 1) {
-                    continue;
-                  } else {
-                    newdata = newdata.replace(`${strarr[i]}`, `console.log("%c ${strarr_[i]}", 'color: ${colors[Math.floor(Math.random() * colors.length)]};')`);
-                    console.log(chalk.blue("logs are now colorful!"))
-                    if (err) console.log(err);
-                    fs.writeFile(filePath, newdata, function (err) {
-                      if (err) {
-                        return console.log(err)
-                      } 
-                    });
-                  }
-                }
-              }
-            }
-          })
-        })
-      })
-    }
-}
-
-module.exports = chalkWebpack;
